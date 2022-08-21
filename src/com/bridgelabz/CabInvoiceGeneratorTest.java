1bab3c60be18bdf70475c7fc767cf4d24eb0c740
KetanC1211 <107634444+KetanC1211@users.noreply.github.com>
2022-08-21 23:44:39 +0530

    Update CabInvoiceGeneratorTest.java

--- src/com/bridgelabz/CabInvoiceGeneratorTest.java
+++ src/com/bridgelabz/CabInvoiceGeneratorTest.java
@@ -13,4 +13,10 @@ public class CabInvoiceGeneratorTest {
 		Assert.assertEquals(120.0,c.getInvoice(10, 20), 0);
 		
 	}
+	
+	@Test
+	public void test1() {
+		CabInvoiceGenerator c = new CabInvoiceGenerator();
+		Assert.assertEquals(240.0,c.getInvoice(20, 40), 0);
+	}
 }
