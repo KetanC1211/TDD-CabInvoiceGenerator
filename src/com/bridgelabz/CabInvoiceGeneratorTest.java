bffc8073a44cbbf44b47157abe88bf1171d0f2b8
KetanC1211 <ketanchindarkar12@gmail.com>
2022-08-21 23:22:18 +0530

    Added files

---
+++ src/com/bridgelabz/CabInvoiceGeneratorTest.java
@@ -0,0 +1,14 @@
+package com.bridgelabz;
+
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CabInvoiceGeneratorTest {
+
+	@Test
+	public void test() {
+		
+	}
+}
